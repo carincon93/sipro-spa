faa91313af2e68ee45f881e6fb99b5f8567c8066
Cristian Rincón <carincon93@gmail.com>
2021-06-02 01:27:06 -0500

    Idi model error fixed

--- app/Models/Idi.php
+++ app/Models/Idi.php
@@ -202,7 +202,7 @@ class Idi extends Model
     {
         $user = Auth::user();
         if ($user->hasRole(1)) {
-            $idi = $this::select('idi.id', 'idi.titulo', 'idi.fecha_inicio', 'idi.fecha_finalizacion')->join('proyectos', 'idi.id', 'proyectos.id')->where('proyectos.convocatoria_id', $convocatoria->id)->orderBy('titulo', 'ASC')
+            $idi = Idi::select('idi.id', 'idi.titulo', 'idi.fecha_inicio', 'idi.fecha_finalizacion')->join('proyectos', 'idi.id', 'proyectos.id')->where('proyectos.convocatoria_id', $convocatoria->id)->orderBy('titulo', 'ASC')
                 ->filterIdi(request()->only('search'))->paginate();
         } else {
             $idi = Idi::select('idi.id', 'idi.titulo', 'idi.fecha_inicio', 'idi.fecha_finalizacion')
