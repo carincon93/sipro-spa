49ac96ccf5618cbf41e3d7bad600b2e173b6ccac
Cristian Rincón <carincon93@gmail.com>
2021-06-03 16:43:43 -0500

    Add Cuarto objetivo especifico into cadena valor

--- resources/js/Pages/Convocatorias/Proyectos/CadenaValor/Index.svelte
+++ resources/js/Pages/Convocatorias/Proyectos/CadenaValor/Index.svelte
@@ -41,6 +41,7 @@
         data.addRows([[{ v: 'Primer objetivo específico', f: '<strong>Primer objetivo específico</strong><div>' + objetivos['Primer objetivo específico'] + '</div>' }, 'Objetivo general', 'Primer objetivo específico']])
         data.addRows([[{ v: 'Segundo objetivo específico', f: '<strong>Segundo objetivo específico</strong><div>' + objetivos['Segundo objetivo específico'] + '</div>' }, 'Objetivo general', 'Segundo objetivo específico']])
         data.addRows([[{ v: 'Tercer objetivo específico', f: '<strong>Tercer objetivo específico</strong><div>' + objetivos['Tercer objetivo específico'] + '</div>' }, 'Objetivo general', 'Tercer objetivo específico']])
+        data.addRows([[{ v: 'Cuarto objetivo específico', f: '<strong>Cuarto objetivo específico</strong><div>' + objetivos['Cuarto objetivo específico'] + '</div>' }, 'Objetivo general', 'Cuarto objetivo específico']])
 
         productos.map((producto) => {
             data.addRows([[{ v: producto.v, f: '<strong>Producto</strong><div>' + producto.f + '</div>' }, producto.fkey, producto.tooltip]])
