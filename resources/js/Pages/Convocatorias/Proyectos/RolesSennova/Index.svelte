24382b9c0ee42a6b99e54ca6accfc9f20d5a4afc
Cristian Rincón <carincon93@gmail.com>
2021-06-03 17:16:10 -0500

    texto modified

--- resources/js/Pages/Convocatorias/Proyectos/RolesSennova/Index.svelte
+++ resources/js/Pages/Convocatorias/Proyectos/RolesSennova/Index.svelte
@@ -34,7 +34,7 @@
         <div slot="title">Roles SENNOVA</div>
 
         <h2 class="text-center mt-10 mb-24" slot="caption">
-            Ingrese cada uno de los roles SENNOVA requiere el proyecto. Actualmente el total del costo de los roles SENNOVA es: ${new Intl.NumberFormat('de-DE').format(!isNaN(proyecto.total_roles_sennova) ? proyecto.total_roles_sennova : 0)} COP
+            Ingrese cada uno de los roles SENNOVA que requiere el proyecto. Actualmente el total del costo de los roles SENNOVA es: ${new Intl.NumberFormat('de-DE').format(!isNaN(proyecto.total_roles_sennova) ? proyecto.total_roles_sennova : 0)} COP
         </h2>
 
         <div slot="actions">
