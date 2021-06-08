23ce6a54f9cd3820d7886f6aa87ba85ca9f6adbb
Cristian Rincón <carincon93@gmail.com>
2021-06-07 22:03:57 -0500

    dashboard dialog added

--- resources/js/Pages/Dashboard.svelte
+++ resources/js/Pages/Dashboard.svelte
@@ -3,11 +3,15 @@
     import { inertia, page } from '@inertiajs/inertia-svelte'
     import { route, checkRole, checkPermission } from '@/Utils'
     import { _ } from 'svelte-i18n'
-    import Button from '@/Shared/Button'
     import { Inertia } from '@inertiajs/inertia'
+
+    import Button from '@/Shared/Button'
+    import Dialog from '@/Shared/Dialog'
 
     let authUser = $page.props.auth.user
     let isSuperAdmin = checkRole(authUser, [1])
+
+    let dialogOpen = true
 </script>
 
 <AuthenticatedLayout>
@@ -110,4 +114,69 @@
             {/if}
         </div>
     </div>
+
+    <Dialog bind:open={dialogOpen} id="novedades">
+        <div slot="title" class="flex items-center flex-col mb-10">
+            <figure>
+                <img src={window.basePath + '/images/megaphone.png'} alt="Megafono" />
+            </figure>
+            Novedades
+        </div>
+        <div slot="content">
+            <small>Junio 8</small>
+            <div>
+                <h1 class="text-purple-200 mb-4">Cambiar contraseña</h1>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 149 31" class="w-1/4">
+                    <defs>
+                        <style>
+                            .cls-1 {
+                                fill: #fefefe;
+                            }
+                            .cls-2 {
+                                fill: #cecece;
+                            }
+                        </style>
+                    </defs>
+                    <g>
+                        <g>
+                            <rect class="cls-1" width="149" height="31" rx="2.25" />
+                            <path class="cls-2" d="M33.89,22.28l-2.32-3.87-2.31,3.87-2-1.13,2.28-3.82H25.24V15.12h4.37l-2.3-3.84,1.93-1.13L31.57,14l2.32-3.89,1.93,1.13-2.29,3.84h4.23v2.21H33.54l2.28,3.82Z" /><path class="cls-2" d="M47.88,22.28l-2.32-3.87-2.3,3.87-2-1.13,2.29-3.82H39.23V15.12H43.6l-2.3-3.84,1.93-1.13L45.56,14l2.32-3.89,1.93,1.13-2.29,3.84h4.23v2.21H47.53l2.28,3.82Z" />
+                            <path class="cls-2" d="M61.87,22.28l-2.31-3.87-2.31,3.87-2-1.13,2.29-3.82H53.22V15.12h4.37l-2.3-3.84,1.94-1.13L59.55,14l2.32-3.89,1.94,1.13-2.29,3.84h4.22v2.21H61.52l2.29,3.82Z" /><path class="cls-2" d="M75.87,22.28l-2.32-3.87-2.31,3.87-2-1.13,2.29-3.82H67.21V15.12h4.37l-2.3-3.84,1.94-1.13L73.54,14l2.33-3.89,1.93,1.13-2.29,3.84h4.22v2.21H75.51l2.29,3.82Z" />
+                            <path class="cls-2" d="M89.86,22.28l-2.32-3.87-2.31,3.87-2-1.13,2.28-3.82H81.21V15.12h4.37l-2.3-3.84,1.93-1.13L87.54,14l2.32-3.89,1.93,1.13L89.5,15.12h4.23v2.21H89.51l2.28,3.82Z" />
+                            <path class="cls-2" d="M103.85,22.28l-2.31-3.87-2.31,3.87-2-1.13,2.29-3.82H95.2V15.12h4.37l-2.3-3.84,1.93-1.13L101.53,14l2.32-3.89,1.94,1.13-2.3,3.84h4.23v2.21H103.5l2.29,3.82Z" /><path class="cls-2" d="M117.84,22.28l-2.31-3.87-2.31,3.87-2-1.13,2.29-3.82h-4.36V15.12h4.37l-2.3-3.84,1.94-1.13L115.52,14l2.32-3.89,1.94,1.13-2.29,3.84h4.22v2.21h-4.22l2.29,3.82Z" />
+                        </g>
+                    </g>
+                </svg>
+                <p>Diríjase a la opción <strong>Cambiar contraseña</strong> dando clic en su nombre de usuario ubicado en la parte superior derecha.</p>
+            </div>
+            <hr class="mt-10 mb-10" />
+            <div>
+                <h1 class="text-purple-200 mb-4">Reportar errores</h1>
+                <p>Si desea notificar un error diríjase a la opción <strong>Soporte</strong> dando clic en su nombre de usuario ubicado en la parte superior derecha.</p>
+            </div>
+        </div>
+        <div slot="actions">
+            <div class="p-4">
+                <Button variant="raised" on:click={(event) => (dialogOpen = false)}>Entendido</Button>
+            </div>
+        </div>
+    </Dialog>
 </AuthenticatedLayout>
+
+<style>
+    :global(#novedades-dialog .mdc-dialog__surface) {
+        background-color: #4f46e5;
+    }
+
+    :global(#novedades-dialog .mdc-button) {
+        background-color: #fff;
+    }
+
+    :global(#novedades-mandatory-content, #novedades-mandatory-title) {
+        color: #fff;
+    }
+
+    :global(#novedades-dialog .mdc-button--raised:not(:disabled)) {
+        color: #4f46e5;
+    }
+</style>
