15fe413d5d6cd56fdbf25f2a71b4ad132a1284ed
Cristian Rincón <carincon93@gmail.com>
2021-07-28 14:24:01 -0500

    DB added in TP

--- app/Http/Controllers/TpController.php
+++ app/Http/Controllers/TpController.php
@@ -11,6 +11,7 @@ use App\Models\NodoTecnoparque;
 use App\Models\Regional;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
