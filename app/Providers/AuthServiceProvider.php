97b3bd9ad4fe6aa06a7226c47c435469e989a692
Cristian Rincón <carincon93@gmail.com>
2021-08-01 22:06:14 -0500

    index ta fixed

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,14 +31,14 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerSuperAdminPolicy();
 
         Gate::define('listar-convocatorias', function (User $user) {
-            return $user->getAllPermissions()->whereIn('id', [1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 15, 20, 21])->count() > 0;
+            return $user->getAllPermissions()->whereIn('id', [1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])->count() > 0;
         });
 
         Gate::define('formular-proyecto', function (User $user, $lineaProgramaticaId) {
 
             $lineaProgramatica = LineaProgramatica::find($lineaProgramaticaId);
 
-            if ($lineaProgramatica && $user->getAllPermissions()->whereIn('id', [1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 15, 20, 21])->count() > 0) {
+            if ($lineaProgramatica && $user->getAllPermissions()->whereIn('id', [1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])->count() > 0) {
                 $fechaActual = date('Y-m-d');
                 $convocatoriaActiva = Convocatoria::where('esta_activa', 1)->first();
                 if ($lineaProgramatica->id == 1 && $fechaActual >= $convocatoriaActiva->fecha_inicio_convocatoria_idi && $fechaActual <= $convocatoriaActiva->fecha_finalizacion_convocatoria_idi) {
@@ -59,7 +59,7 @@ class AuthServiceProvider extends ServiceProvider
                     return true;
                 }
             } else {
-                return $user->getAllPermissions()->whereIn('id', [1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 15, 20, 21])->count() > 0;
+                return $user->getAllPermissions()->whereIn('id', [1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])->count() > 0;
             }
 
             return false;
