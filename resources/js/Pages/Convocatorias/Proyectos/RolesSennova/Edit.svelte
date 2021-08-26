7e0e567351e1befef845e71b152e8e58a2d5dcf3
Cristian Rincón <carincon93@gmail.com>
2021-08-26 13:16:33 -0500

    Perfil predeterminado modificable - Roles

--- resources/js/Pages/Convocatorias/Proyectos/RolesSennova/Edit.svelte
+++ resources/js/Pages/Convocatorias/Proyectos/RolesSennova/Edit.svelte
@@ -54,10 +54,8 @@
         }
     }
 
-    let diff_meses = proyecto.diff_meses
     $: if ($form.convocatoria_rol_sennova_id) {
         if (proyecto.codigo_linea_programatica == 68) {
-            $form.descripcion = infoRolSennova?.perfil == null ? $form.descripcion : infoRolSennova?.perfil
             if ($form.convocatoria_rol_sennova_id == 108) {
                 $form.numero_meses = 6
             } else {
@@ -67,7 +65,13 @@
     }
 
     $: if (infoRolSennova?.perfil != null) {
-        $form.descripcion = infoRolSennova.perfil
+        if (infoRolSennova?.value == proyectoRolSennova.convocatoria_rol_sennova_id) {
+            $form.descripcion = proyectoRolSennova.descripcion
+        } else {
+            $form.descripcion = infoRolSennova.perfil
+        }
+    } else {
+        $form.descripcion = ''
     }
 </script>
 
@@ -97,7 +101,7 @@
 
                     <div class="mt-4">
                         {#if infoRolSennova?.perfil}
-                            <Textarea disabled label="Descripción" maxlength="40000" id="descripcion" error={errors.descripcion} value={infoRolSennova.perfil} required />
+                            <Textarea disabled={proyecto.codigo_linea_programatica != 68} label="Descripción" maxlength="40000" id="descripcion" error={errors.descripcion} bind:value={$form.descripcion} required />
                         {:else}
                             <Textarea label="Descripción" maxlength="40000" id="descripcion" error={errors.descripcion} bind:value={$form.descripcion} required />
                         {/if}
