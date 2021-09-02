d2d3998733d4966567692f7cc622ff155dd1a208
Jorge Carrillo <jorge940903@hotmail.com>
2021-09-02 11:11:50 -0500

    Configuracion path cookie laravel

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,4 +14,23 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         ''
     ];
+
+    /**
+     * Add the CSRF token to the response cookies.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Symfony\Component\HttpFoundation\Response  $response
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function addCookieToResponse($request, $response)
+    {
+        $config = config('session');
+        $response->headers->setCookie(
+            new Cookie(
+                'XSRF-TOKEN', $request->session()->token(), Carbon::now()->getTimestamp() + 60 * $config['lifetime'],
+                $config['path'], $config['domain'], $config['secure'], $config['http_only']
+            )
+        );
+        return $response;
+    }
 }
