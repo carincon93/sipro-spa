e37414b58c83ddfd561184cb5e160fc2295a49ae
Jorge Carrillo <jorge940903@hotmail.com>
2021-09-02 15:08:42 -0500

    Configuracion path cookie laravel

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 use Symfony\Component\HttpFoundation\Cookie;
+use \Carbon\Carbon;
 
 class VerifyCsrfToken extends Middleware
 {
