c7aa9a8ff924f1e479270a67c60069cd22d24b1e
Jorge Carrillo <jorge940903@hotmail.com>
2021-09-20 15:35:47 -0500

    Reportes: Resumen presupuestos y roles SENNOVA

---
+++ app/Exports/PresupuestosExport.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\ProyectoPresupuesto;
+use App\Models\Convocatoria;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Concerns\WithProperties;
+
+class PresupuestosExport implements FromCollection, WithHeadings, WithMapping, WithStyles, WithProperties, WithColumnFormatting, WithTitle
+{
+    protected $datos;
+    protected $convocatoria;
+
+    public function __construct(Convocatoria $convocatoria)
+    {
+        $this->convocatoria = $convocatoria;
+    }
+
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        $idproyectos = explode(',', $this->convocatoria->proyectos->implode('id', ','));
+        return ProyectoPresupuesto::whereIn('proyecto_id', $idproyectos)->with('convocatoriaPresupuesto.presupuestoSennova.tercerGrupoPresupuestal:id,nombre', 'convocatoriaPresupuesto.presupuestoSennova.segundoGrupoPresupuestal:id,nombre,codigo', 'convocatoriaPresupuesto.presupuestoSennova.usoPresupuestal:id,descripcion')->get();
+    }
+
+    /**
+     * @var Invoice $presupuesto
+     */
+    public function map($presupuesto): array
+    {
+        return [
+            $presupuesto->proyecto->convocatoria->descripcion,
+            $presupuesto->proyecto->codigo,
+            $presupuesto->convocatoriaPresupuesto->presupuestoSennova->tercerGrupoPresupuestal->nombre,
+            $presupuesto->convocatoriaPresupuesto->presupuestoSennova->segundoGrupoPresupuestal->nombre,
+            $presupuesto->convocatoriaPresupuesto->presupuestoSennova->usoPresupuestal->nombre,
+            $presupuesto->descripcion,
+            $presupuesto->justificacion,
+            $presupuesto->valor_total,
+
+        ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Convocatoria',
+            'Código',
+            'Tercer Grupo Presupuestal',
+            'Segundo Grupo Presupuestal',
+            'Uso Presupuestal',
+            'Descripción',
+            'Justificación',
+            'Valor',
+        ];
+    }
+
+    public function columnFormats(): array
+    {
+        return [
+            'H' => NumberFormat::FORMAT_CURRENCY_USD_SIMPLE,
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function title(): string
+    {
+        return 'Resumen Presupuestal';
+    }
+
+    public function properties(): array
+    {
+        return [
+            'title' => 'Resumen Presupuestal ' . $this->convocatoria->descripcion,
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            // Style the first row as bold text.
+            1    => ['font' => ['bold' => true]],
+        ];
+    }
+}
