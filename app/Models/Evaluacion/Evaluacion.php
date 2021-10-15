f2f65cc24167f4dc973557a93a300d78fa342d5d
Cristian Rincón <carincon93@gmail.com>
2021-10-15 13:49:52 -0500

    Evaluaciones buscador fixed

--- app/Models/Evaluacion/Evaluacion.php
+++ app/Models/Evaluacion/Evaluacion.php
@@ -523,10 +523,16 @@ class Evaluacion extends Model
     public function scopeFilterEvaluacion($query, array $filters)
     {
         $query->when($filters['search'] ?? null, function ($query, $search) {
+            $search = str_replace('"', "", $search);
+            $search = str_replace("'", "", $search);
+            $search = str_replace(' ', '%%', $search);
+
             $query->select('users.nombre', 'evaluaciones.*');
             $query->join('users', 'evaluaciones.user_id', 'users.id');
             $query->where('users.nombre', 'ilike', '%' . $search . '%');
-            $query->orWhere('evaluaciones.proyecto_id', $search - 8000);
+            if (is_numeric($search)) {
+                $query->orWhere('evaluaciones.proyecto_id', $search - 8000);
+            }
         })->when($filters['estado'] ?? null, function ($query, $estado) {
             if ($estado == 'finalizados idi') {
                 $query->join('idi_evaluaciones', 'evaluaciones.id', 'idi_evaluaciones.id');
