eb27db7bc4c4622f8b9bf7cd7eed9ccd52cdd5f7
Cristian Rincón <carincon93@gmail.com>
2021-12-16 12:27:30 -0500

    js

---
+++ public/js/2885.js
@@ -0,0 +1,1 @@
+(self.webpackChunk=self.webpackChunk||[]).push([[2885],{72885:(s,e,u)=>{"use strict";u.r(e),u.d(e,{default:()=>c});var l=u(21379);u(52218);class t extends l.f_{constructor(s){super(),(0,l.S1)(this,s,null,null,l.N8,{})}}const c=t}}]);
